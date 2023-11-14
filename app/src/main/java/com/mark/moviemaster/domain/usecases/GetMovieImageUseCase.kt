f0abae8574e51589345fc2f3f8a1ae1aab4e1dda
markoeltiger <mkhalil@trycar.com>
2023-11-14 18:30:29 +0200

    Added Movie Title Related Images from Flicker

--- app/src/main/java/com/mark/moviemaster/domain/usecases/GetMovieImageUseCase.kt
+++ app/src/main/java/com/mark/moviemaster/domain/usecases/GetMovieImageUseCase.kt
@@ -1,5 +1,6 @@
 package com.mark.moviemaster.domain.usecases
 
+import com.mark.moviemaster.data.remote.models.MovieImageResponse
 import com.mark.moviemaster.domain.repos.MovieImageRepository
 import com.mark.moviemaster.utils.helper.AppHelper.getMovieImageLink
 import javax.inject.Inject
@@ -7,10 +8,10 @@ import javax.inject.Inject
 class GetMovieImageUseCase @Inject constructor(
     private val movieImageRepository: MovieImageRepository,
 ) {
-    suspend fun getAllMoviesUseCase(movieTitle:String):String{
-        var image = movieImageRepository.getMovieImage(movieTitle).photos.photo.first()
-        var imageLink = getMovieImageLink(image.farm,image.server,image.id,image.secret)
-         return imageLink
+    suspend fun getAllMoviesUseCase(movieTitle:String):MovieImageResponse{
+
+      //  var imageLink = getMovieImageLink(image.farm,image.server,image.id,image.secret)
+         return  movieImageRepository.getMovieImage(movieTitle)
 
     }
 }
--- app/src/main/java/com/mark/moviemaster/presentation/ui/moviedetails/MovieDetailsScreen.kt
+++ app/src/main/java/com/mark/moviemaster/presentation/ui/moviedetails/MovieDetailsScreen.kt
@@ -1,11 +1,13 @@
 package com.mark.moviemaster.presentation.ui.moviedetails
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,9 +16,13 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,21 +32,35 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.mark.moviemaster.presentation.ui.components.MovieListItem
-import com.mark.moviemaster.presentation.ui.navigation.Routes
+import com.mark.moviemaster.R
 import com.mark.moviemaster.presentation.viewmodel.movieimage.MovieDetailsViewModel
+import com.mark.moviemaster.presentation.viewmodel.movieimage.ui_state.MovieUiState
 import com.mark.moviemaster.utils.helper.AppHelper
+import com.mark.moviemaster.utils.helper.AppHelper.getMovieImageLink
 import com.mark.moviemaster.utils.helper.AppHelper.random
 import com.skydoves.landscapist.CircularReveal
 import com.skydoves.landscapist.ShimmerParams
 import com.skydoves.landscapist.glide.GlideImage
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
 
 @Composable
 fun MovieDetailsScreen(
-    navController: NavController,
-    viewModel: MovieDetailsViewModel = hiltViewModel()
+    navController: NavController, viewModel: MovieDetailsViewModel = hiltViewModel()
 ) {
-    val movieUiState by viewModel.movieDetail.collectAsState()
+    var state: MovieUiState by remember { mutableStateOf(MovieUiState()) }
+    //Launched Effect to stop calling the function when recomposation
+    LaunchedEffect(true) {
+        viewModel.movieDetail(AppHelper.selectedMovie!!.title)
+
+        launch {
+            state = viewModel.movieDetail.value
+        }
+        viewModel.movieDetail.collectLatest {
+            state = it
+        }
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -48,18 +68,81 @@ fun MovieDetailsScreen(
                 Color.White
             )
     ) {
-        UiDetail()
-        viewModel.movieDetail(AppHelper.selectedMovie!!.title)
-        viewModel.movieDetail.collectAsState().value?.let {
-            when (it) {
+        Column {
+            MovieDetail()
+            state.photos.let {
+                Row {
+                    LazyColumn {
+                        items(it.size / 2) { item ->
+                            var image = state.photos.get(item + 10)
+                            GlideImage(
+                                imageModel = getMovieImageLink(
+                                    image.farm,
+                                    image.server,
+                                    image.id,
+                                    image.secret
+                                ),
+                                contentScale = ContentScale.Crop,
+                                circularReveal = CircularReveal(duration = 100),
+                                alignment = Alignment.Center,
 
+                                shimmerParams = ShimmerParams(
+                                    baseColor = Color.LightGray,
+                                    highlightColor = Color.Gray,
+                                    durationMillis = 500,
+                                    dropOff = 0.55f,
+                                    tilt = 20f
+
+                                ),
+                                modifier = Modifier
+                                    .width(200.dp)
+                                    .height(100.dp)
+                                    .padding(4.dp)
+                            )
+
+                        }
+                    }
+                    LazyColumn {
+
+                        items(it.size / 2) { item ->
+                            var image = state.photos.get(item)
+                            GlideImage(
+
+                                imageModel = getMovieImageLink(
+                                    image.farm,
+                                    image.server,
+                                    image.id,
+                                    image.secret
+                                ),
+
+                                circularReveal = CircularReveal(duration = 100),
+                                alignment = Alignment.Center,
+
+                                shimmerParams = ShimmerParams(
+                                    baseColor = Color.LightGray,
+                                    highlightColor = Color.Gray,
+                                    durationMillis = 500,
+                                    dropOff = 0.55f,
+                                    tilt = 20f
+
+                                ),
+                                modifier = Modifier
+                                    .width(200.dp)
+                                    .height(100.dp)
+                                    .padding(4.dp)
+                            )
+
+                        }
+                    }
+                }
             }
         }
+
     }
 }
-
 @Composable
-fun UiDetail() {
+fun MovieDetail(
+) {
     var selectedMovie = AppHelper.selectedMovie
     Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
         Column(
@@ -72,64 +155,25 @@ fun UiDetail() {
                     .fillMaxWidth()
                     .padding(bottom = 10.dp, top = 10.dp)
             ) {
-                Text(
-                    text = "Movie Name :  ",
-                    modifier = Modifier.padding(top = 10.dp),
-                    color = Color.Black,
-                    fontSize = 17.sp,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
-                )
+
                 Text(
                     text = "${selectedMovie!!.title} (${selectedMovie.year})",
                     modifier = Modifier.padding(top = 10.dp),
                     color = Color.Black,
-                    fontSize = 17.sp,
+                    fontSize = 20.sp,
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis
                 )
             }
-if (selectedMovie!!.cast.size>1){
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 10.dp, top = 10.dp)
-            ) {
-                Text(
-                    text = "Cast : ",
-                     color = Color.Black,
-                    fontSize = 17.sp,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
-                )
-                LazyRow(
-                    modifier = Modifier.align(CenterVertically),
-                ) {
-                    items(selectedMovie!!.cast.size) { item ->
-                        Text(
-                            text = "${selectedMovie?.cast?.get(item)}",
-                            modifier = Modifier.align(CenterVertically),
-                            color = Color.Companion.random(),
-                            fontSize = 17.sp,
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
-                        Spacer(modifier = Modifier.width(2.dp))
-                    }
-
-                }
-            }
-}
-            if (selectedMovie!!.genres.size>1) {
-
+            if (selectedMovie!!.cast.size > 1) {
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(bottom = 10.dp, top = 10.dp)
                 ) {
                     Text(
-                        text = "Genres : ",
-                         color = Color.Black,
+                        text = "Cast : ",
+                        color = Color.Black,
                         fontSize = 17.sp,
                         maxLines = 1,
                         overflow = TextOverflow.Ellipsis
@@ -137,24 +181,59 @@ if (selectedMovie!!.cast.size>1){
                     LazyRow(
                         modifier = Modifier.align(CenterVertically),
                     ) {
-                        items(selectedMovie!!.cast.size) { item ->
+                        items(selectedMovie.cast.size) { item ->
                             Text(
-                                text = "${selectedMovie!!.genres.get(item)}",
+                                text = "${selectedMovie.cast?.get(item)}",
                                 modifier = Modifier.align(CenterVertically),
                                 color = Color.Companion.random(),
                                 fontSize = 17.sp,
                                 maxLines = 1,
                                 overflow = TextOverflow.Ellipsis
                             )
-                            Spacer(modifier = Modifier.width(2.dp))
-
+                            Spacer(modifier = Modifier.width(4.dp))
                         }
-
                     }
                 }
             }
+            if (selectedMovie.genres.size > 1) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 10.dp, top = 10.dp)
+                ) {
+                    Text(
+                        text = "Genres : ",
+                        color = Color.Black,
+                        fontSize = 17.sp,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    LazyRow(
+                        modifier = Modifier.align(CenterVertically),
+                    ) {
+                        items(selectedMovie.genres.size) { item ->
+                            Text(
+                                text = "${selectedMovie.genres.get(item)}",
+                                modifier = Modifier.align(CenterVertically),
+                                color = Color.Companion.random(),
+                                fontSize = 17.sp,
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis
+                            )
+                            Spacer(modifier = Modifier.width(4.dp))
+                        }
+                    }
+                }
+            }
+            Text(
+                text = "Movie Images",
+                color = Color.Black,
+                modifier = Modifier.align(CenterHorizontally),
+                fontSize = 20.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
         }
-
     }
 }
 
